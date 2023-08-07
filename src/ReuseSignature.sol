e25ff56bf8509cd5e93101fc00792c08eebd2107
Ratimon <ratixoxo@gmail.com>
2023-08-07 19:07:49 +0700

    Build(./src): Adding ReuseSignature challenge#1

---
+++ src/ReuseSignature.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.19;
+
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract ReuseSignature {
+	using ECDSA for bytes32;
+    address public verifyingAddress = 0x0000000cCC7439F4972897cCd70994123e0921bC;
+    mapping(bytes => bool) public used;
+
+	function challenge(
+        string calldata message,
+        bytes calldata signature
+    ) public {
+        bytes32 signedMessageHash = keccak256(abi.encode(message))
+            .toEthSignedMessageHash();
+        require(
+            signedMessageHash.recover(signature) == verifyingAddress,
+            "signature not valid"
+        );
+        
+        require(!used[signature]);
+        used[signature] = true;
+   }
+}
