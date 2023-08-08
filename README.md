29530bfaea6945c640d69894255fcd1a07fded04
Ratimon <ratixoxo@gmail.com>
2023-08-08 09:27:40 +0700

    Build(./): Integrating hardhat

--- README.md
+++ README.md
@@ -3,3 +3,60 @@
 ```
 forge install openzeppelin/openzeppelin-contracts@v4.9.3
 ```
+
+## Integrating hardhat
+
+1. 
+
+```
+pnpm add -D hardhat	
+```
+2. 
+
+```
+npx hardhat
+```
+3. Add this to your `hardhat.config.ts` file
+
+```ts
+import fs from "fs";
+import "hardhat-preprocessor";
+```
+4. Ensure the following function is present
+
+```ts
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+```
+
+5. Add the following to your exported HardhatUserConfig object:
+
+```ts
+preprocess: {
+  eachLine: (hre) => ({
+    transform: (line: string) => {
+      if (line.match(/^\s*import /i)) {
+        for (const [from, to] of getRemappings()) {
+          if (line.includes(from)) {
+            line = line.replace(from, to);
+            break;
+          }
+        }
+      }
+      return line;
+    },
+  }),
+},
+paths: {
+  sources: "./src",
+  cache: "./cache_hardhat",
+},
+```
+
+
+
---
+++ hardhat.config.ts
@@ -0,0 +1,63 @@
+import { HardhatUserConfig } from "hardhat/config";
+import fs from "fs";
+import "hardhat-preprocessor"
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+
+    compilers: 
+  [
+    {
+      version: "0.8.19",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        outputSelection: {
+          "*": {
+              "*": ["storageLayout"],
+          },
+        },
+      }
+    }
+  ]
+  },
+
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      }
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+
+};
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+
+
+export default config;
